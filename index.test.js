338a07b527551b97d64f7f1f5ba383050ea7981a
Mieli <alessandro.mieli@gmail.com>
2023-10-17 22:32:10 -0300

    testes e arquivo index refatorado

--- index.test.js
+++ index.test.js
@@ -5,13 +5,7 @@ test("testar o retorno da função para calcular o saldo de rankeadas que deve s
 
     const vitorias = 10;
     const derrotas = 2;
-
-    let rankeadas = calcularRankeamento(vitorias, derrotas);
-
-    const expected = (vitorias - derrotas);
-
-
-    expect(rankeadas).toBe(expected);
+    expect(calcularRankeamento(vitorias, derrotas)).toBe(vitorias - derrotas);
 
 });
 
@@ -19,51 +13,56 @@ test("deve criar uma mensagem do saldo de vitórias e do nível do herói", () =
 
     const saldoVitorias = 10;
     const nivel = 4;
-
-    const mensagem = criarMensagemDoHeroi(saldoVitorias, nivel);
-
-    expect(mensagem).toBe(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+    expect(criarMensagemDoHeroi(saldoVitorias, nivel)).toBe(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
 
 });
 
-test('deve retornar o nivel do herói vazio', () => {
-    const vitorias = 10;
-
-    const nivel = calcularNivelDoHeroi(vitorias);
-
-    expect(nivel).toBe('');
-});
 
 test('deve retornar o nivel do herói Ferro', () => {
-    const vitorias = 9;
-
-    const nivel = calcularNivelDoHeroi(vitorias);
-
-    const expected = 'Ferro'
-
-    expect(nivel).toBe(expected);
+    expect(calcularNivelDoHeroi(9)).toBe('Ferro');
 });
 
 test('deve retornar o nivel do herói Bronze', () => {
-    const vitorias = 11;
-
-    const nivel = calcularNivelDoHeroi(vitorias);
-
-    expect(nivel).toBe('Bronze');
+    expect(calcularNivelDoHeroi(11)).toBe('Bronze');
 });
 
 test('deve retornar o nivel do herói Bronze', () => {
-    const vitorias = 20;
-
-    const nivel = calcularNivelDoHeroi(vitorias);
-
-    expect(nivel).toBe('Bronze');
+    expect(calcularNivelDoHeroi(20)).toBe('Bronze');
 });
 
 test('deve retornar o nivel do herói Bronze', () => {
-    const vitorias = 15;
+    expect(calcularNivelDoHeroi(15)).toBe('Bronze');
+});
 
-    const nivel = calcularNivelDoHeroi(vitorias);
 
-    expect(nivel).toBe('Bronze');
+test('Teste para o nível Ferro', () => {
+    expect(calcularNivelDoHeroi(5)).toBe("Ferro");
+});
+
+test('Teste para o nível Bronze', () => {
+    expect(calcularNivelDoHeroi(15)).toBe("Bronze");
+});
+
+test('Teste para o nível Prata', () => {
+    expect(calcularNivelDoHeroi(30)).toBe("Prata");
+});
+
+test('Teste para o nível Ouro', () => {
+    expect(calcularNivelDoHeroi(70)).toBe("Ouro");
+});
+
+test('Teste para o nível Diamante', () => {
+    expect(calcularNivelDoHeroi(85)).toBe("Diamante");
+});
+
+test('Teste para o nível Lendário', () => {
+    expect(calcularNivelDoHeroi(95)).toBe("Lendário");
+});
+
+test('Teste para o nível Imortal', () => {
+    expect(calcularNivelDoHeroi(110)).toBe("Imortal");
+});
+
+test('Teste para o nível Imortal', () => {
+    expect(calcularNivelDoHeroi(101)).toBe("Imortal");
 });
